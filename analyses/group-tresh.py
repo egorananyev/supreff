3cdb515ae83920fdb8b8929d83b2f6f99d4f4856
egorananyev <egor.ananyev@gmail.com>
2015-04-14 17:57:52 +0800

    Finished plotting individual thresholds based on the aggregated data set. Created box plots with the information gathered across subjects. Still need to plot normalized values.

---
+++ analyses/group-tresh.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python2
+
+from __future__ import division
+from psychopy import data, gui, core
+from psychopy.tools.filetools import fromFile
+import pylab, scipy
+import os, glob # file and directory handling
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# CONSTANTS.
+nRevs = 4
+
+# Selecting the files for processing.
+dataDir = '..' + os.sep + 'data' # common data directory
+allSubjDirs = os.walk(dataDir).next()[1] # get the directory names
+print allSubjDirs
+
+## Nested loop to go through the directories and files.
+# A single data frame for the entire data set:
+wdf = pd.DataFrame(columns=['subj','session','maskSpeed','targLoc','startVal',
+    'trial','label','contrast'])
+# No, it means "whole threshold frame":
+wtf = pd.DataFrame(columns=['subj','session','maskSpeed','targLoc','startVal',
+    'label','threshold'])
+# Looping through the directories:
+for thisSubjDir in allSubjDirs:
+    # CSV file for getting the current subject and session:
+    thisCsv = pd.read_csv(dataDir + os.sep + thisSubjDir + '.csv')
+    thisSubj = thisCsv.participant[0]
+    thisSession = thisCsv.session[0]
+    # The list of files for the current session:
+    files = glob.glob(dataDir + os.sep + thisSubjDir + os.sep + '*.psydat')
+#    print files
+    # Looping through the files:
+    for thisFileName in files:
+        thisDat = fromFile(thisFileName)
+        assert isinstance(thisDat, data.StairHandler) # not sure what this does but oh well
+        nTrials = len(thisDat.intensities)
+        # A local data set to be later appended to the whole data set:
+        df = pd.DataFrame({
+            'subj': np.repeat(thisSubj, nTrials),
+            'session': np.repeat(thisSession, nTrials),
+            'maskSpeed': np.repeat(thisDat.extraInfo['maskSpeed'], nTrials),
+            'targLoc': np.repeat(thisDat.extraInfo['targLoc'], nTrials),
+            'startVal': np.repeat(thisDat.extraInfo['startVal'], nTrials),
+            'trial': range(nTrials),
+            'label': np.repeat(thisDat.extraInfo['label'], nTrials),
+            'contrast': thisDat.intensities})
+        wdf = wdf.append(df)
+        tf = pd.DataFrame({
+            'subj': thisSubj,
+            'session': thisSession,
+            'maskSpeed': thisDat.extraInfo['maskSpeed'],
+            'targLoc': thisDat.extraInfo['targLoc'],
+            'startVal': thisDat.extraInfo['startVal'],
+            'label': thisDat.extraInfo['label'],
+            'threshold': [np.average(thisDat.reversalIntensities[-nRevs:])] })
+        wtf = wtf.append(tf)
+    # getting the subject directory name to use as the name for plots:
+    subjDirName = os.path.basename(os.path.dirname(thisFileName))
+    #wdf.to_csv(os.path.join(os.path.dirname(thisFileName), 'res-table.csv'))
+
+print wdf # trial contrasts
+print wtf # thresholds
+
+# Plotting using seaborn.
+#sns.set(style="ticks")
+#sns.set(style="whitegrid")
+#sns.set(font_scale=1.8)
+#grid = sns.FacetGrid(wtf, col='subj', col_wrap=3, size=5.5)
+#grid.map(plt.factorplot, 'maskSpeed', 'threshold', 'targLoc')
+#grid.set(ylim=(0,1))
+
+sns.factorplot('maskSpeed', 'threshold', 'targLoc', col='subj', data=wtf, col_wrap=3, 
+    kind='bar')
+pylab.show()
+
+sns.factorplot('maskSpeed', 'threshold', 'targLoc', wtf, kind='box')
+pylab.show()
+
+'''
+# Output the figure.
+figFile = dataDir + os.sep + subjDirName + '.pdf'
+pylab.savefig(figFile)
+print 'saved figure to: ' + figFile
+'''
--- analyses/subj-thresh.py
+++ analyses/subj-thresh.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python2
 
-# This analysis script takes one or more staircase datafiles as input from a GUI
-# It then plots the staircases on top of each other on the left 
-# and a combined psychometric function from the same data
-# on the right.
-#
-# The combined plot uses every unique X value form the staircase, and alters the
-# size of the points according to how many trials were run at that level.
-
 from __future__ import division
 from psychopy import data, gui, core
 from psychopy.tools.filetools import fromFile
@@ -18,36 +10,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# conditional variables:
-condVars = ['maskSpeed','targLoc']
-
-# set to 0.5 for Yes/No (or PSE). Set to 0.8 for a 2AFC threshold
-threshVal = 0.8 # 0.5
-# set to zero for Yes/No (or PSE). Set to 0.5 for 2AFC
-expectedMin = 0.5 # 0.0
-
-files = gui.fileOpenDlg('..' + os.sep + 'data')
+# Selecting the files for processing.
+dataDir = '..' + os.sep + 'data' # common data directory
+files = gui.fileOpenDlg(dataDir)
 if not files:
     core.quit()
 
-# get the data from all the files:
-allIntensities, allResponses, allInfos, allCondLabels = [],[],[],[]
-for thisFileName in files:
-    thisDat = fromFile(thisFileName)
-    assert isinstance(thisDat, data.StairHandler)
-    allIntensities.append(thisDat.intensities)
-    allResponses.append(thisDat.data)
-    allInfos.append(thisDat.extraInfo)
-    allCondLabels.append(thisDat.extraInfo['label'])
-#    print '======'
-#    print thisFileName
-#    print thisDat.intensities
-    print thisDat.extraInfo['label']
-#    print thisDat.extraInfo
-    print np.average(thisDat.reversalIntensities[-15:])
-dataFolder = os.path.split(thisFileName)[0] #just the path, excluding file name
-
-# My version of data input:
+# My version of data input.
 print 'BUILDING DATA FRAME'
 wdf=pd.DataFrame(columns=['maskSpeed','targLoc','startVal','trial','label','contrast'])
 for thisFileName in files:
@@ -63,8 +32,10 @@ for thisFileName in files:
         'contrast': thisDat.intensities})
 #    print df
     wdf = wdf.append(df)
-print wdf
-wdf.to_csv(os.path.join(dataFolder, 'res-table.csv'))
+# getting the subject directory name to use as the name for plots:
+subjDirName = os.path.basename(os.path.dirname(thisFileName))
+#print wdf
+wdf.to_csv(os.path.join(os.path.dirname(thisFileName), 'res-table.csv'))
 print 'BUILDING DATA FRAME COMPLETED'
 
 # Plotting using seaborn.
@@ -73,43 +44,9 @@ sns.set(font_scale=1.8)
 grid = sns.FacetGrid(wdf, row='targLoc', col='maskSpeed', hue='startVal', size=5.5)
 grid.map(plt.plot, 'trial', 'contrast', marker='o')
 grid.set(ylim=(0,1))
+
+# Output the figure.
+figFile = dataDir + os.sep + subjDirName + '.pdf'
+pylab.savefig(figFile)
+print 'saved figure to: ' + figFile
 pylab.show()
-
-'''
-# Load the conditions data set.
-cond = pd.read_csv(dataFolder + os.sep + 'cond-expt01.csv')
-
-# My own plotting function.
-# A double-nested loop to go through levels of factors:
-fileN = 0
-colors = 'obrgkcmobrgkcm'
-f, axarr = plt.subplots(2,5)
-#grid = sns.FacetGrid(
-for ixMaskSpeed, curMaskSpeed in enumerate(pd.unique(cond.maskSpeed)):
-    for ixTargLoc, curTargLoc in enumerate(pd.unique(cond.targLoc)):
-        print '======'
-        print allCondLabels
-        # Finding the cond file row matching the current loop iteration:
-        condLow = cond.label[(cond['targLoc']==curTargLoc) & \
-            (cond['maskSpeed']==curMaskSpeed) & \
-            (cond['startVal']==0.1)]
-        # Finding the file with the data for the above condition:
-        fileLow = allCondLabels.index(condLow.values)
-#        print condLow.values
-#        print fileLow
-#        print allIntensities[fileLow]
-        axarr[ixTargLoc, ixMaskSpeed].plot(allIntensities[fileLow])
-        axarr[ixTargLoc, ixMaskSpeed]
-        condHigh = cond.label[(cond['targLoc']==curTargLoc) & \
-            (cond['maskSpeed']==curMaskSpeed) & \
-            (cond['startVal']==0.8)]
-        # Finding the file with the data for the above condition:
-        fileHigh = allCondLabels.index(condHigh.values)
-#        print condHigh.values
-#        print fileHigh
-#        print allIntensities[fileHigh]
-        axarr[ixTargLoc, ixMaskSpeed].plot(allIntensities[fileHigh])
-        axarr[ixTargLoc, ixMaskSpeed].set_title(str(curMaskSpeed)+' '+curTargLoc)
-
-pylab.show()
-'''
